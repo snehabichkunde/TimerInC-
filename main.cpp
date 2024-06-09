f3fddea08fa2a6b3f4600dd78baf1e896695a1e3
SnehaBichunde2003 <2022bit065@sggs.ac.in>
2024-06-09 18:04:40 +0530

    Timer in c++

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <iomanip>
+
+using namespace std;
+
+void timer(int hours, int minutes, int seconds)
+{
+    while (hours > 0 || minutes > 0 || seconds > 0) {
+        cout << "\rTime remaining: " << setw(2) << setfill('0') << hours << "h "
+             << setw(2) << setfill('0') << minutes << "m " 
+             << setw(2) << setfill('0') << seconds << "s"<< flush;
+
+        this_thread::sleep_for(chrono::seconds(1));
+        
+        if (seconds > 0) {
+            seconds--;
+        } else {
+            seconds = 59;
+            if (minutes > 0) {
+                minutes--;
+            } else {
+                minutes = 59;
+                if (hours > 0) {
+                    hours--;
+                }
+            }
+        }
+    }
+    cout << "\rTime's up!                            " << endl; 
+}
+
+bool isValidTime(int hours, int minutes, int seconds)
+{
+    return (hours >= 0 && minutes >= 0 && minutes < 60 && seconds >= 0 && seconds < 60);
+}
+
+int main()
+{
+    int hours, minutes, seconds;
+    cout << "Enter time (hours minutes seconds): ";
+    cin >> hours >> minutes >> seconds;
+
+    if (!isValidTime(hours, minutes, seconds)) {
+        cout << "Invalid time input. Please enter valid values." << endl;
+        return 1; 
+    }
+
+    timer(hours, minutes, seconds);
+
+    return 0;
+}
