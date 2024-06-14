1a114678cae8a9b55a7f57a3036607a0dba2302b
SnehaBichunde2003 <2022bit065@sggs.ac.in>
2024-06-14 17:46:23 +0530

    added stop timer function

--- main.cpp
+++ main.cpp
@@ -3,14 +3,41 @@
 #include <chrono>
 #include <iomanip>
 
+bool is_running = true;
+bool is_stopped = false;
+
 using namespace std;
+
+void stopTimmer(){
+    char input;
+    //cin>>input;
+    while(true){
+        cin>>input;
+        if(input == 'r'){
+            is_running = true;
+            is_stopped = false;
+        }
+        else if (input == 's')
+        {
+            is_running = false;
+            is_stopped = true;
+        }
+        
+    }
+}
 
 void timer(int hours, int minutes, int seconds)
 {
+    
     while (hours > 0 || minutes > 0 || seconds > 0) {
-        cout << "\rTime remaining: " << setw(2) << setfill('0') << hours << "h "
+        while(is_stopped){
+            this_thread:: sleep_for(chrono::seconds(1));
+        }
+        if(is_running)
+        {cout << "\rTime remaining: " << setw(2) << setfill('0') << hours << "h "
              << setw(2) << setfill('0') << minutes << "m " 
              << setw(2) << setfill('0') << seconds << "s"<< flush;
+             //cout << endl;
 
         this_thread::sleep_for(chrono::seconds(1));
         
@@ -27,7 +54,7 @@ void timer(int hours, int minutes, int seconds)
                 }
             }
         }
-    }
+    }}
     cout << "\rTime's up!                            " << endl; 
 }
 
@@ -39,6 +66,7 @@ bool isValidTime(int hours, int minutes, int seconds)
 int main()
 {
     int hours, minutes, seconds;
+    cout << "Enter s to stop the timer and r to restart the timmer: " << endl;
     cout << "Enter time (hours minutes seconds): ";
     cin >> hours >> minutes >> seconds;
 
@@ -46,7 +74,7 @@ int main()
         cout << "Invalid time input. Please enter valid values." << endl;
         return 1; 
     }
-
+    thread userInput(stopTimmer);
     timer(hours, minutes, seconds);
 
     return 0;
